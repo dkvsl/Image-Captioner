dffe14e40a6c9b3645b5a0a7faf8d32bddeaff8f
Dani Darko <hola@darkovisual.com>
2024-06-14 17:32:05 +0200

    Update start_linux_mac.sh

--- start_linux_mac.sh
+++ start_linux_mac.sh
@@ -5,6 +5,4 @@ source myenv/bin/activate
 
 python ./install_script/check_open.py
 
-python gpt-caption.py "$@"
-
-read -p "Press any key to continue . . . "
+python gpt-caption.py "$@" --listen --share
